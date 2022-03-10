ae742ac3a4158e69ab96f1cfc4db08ef0f8fd34f
Gudak0 <diehardsteam@gmail.com>
2022-03-10 15:40:58 -0800

    updated config

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata:size=12" };
+static const char *fonts[]          = { "Inconsolata:size=12", "fontawesome:size=12" };
 static const char dmenufont[]       = "Inconsolata:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "M", "1", "2", "3", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
